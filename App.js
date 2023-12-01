1157f92e8c6dadee222896718e7c61947af31be8
immortal-codes <rajanraj8979@gmail.com>
2023-12-01 17:52:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, Text, TouchableOpacity,StatusBar,} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-export default function App() {
+
+
+const App = () => {
+
+  const [Quote, setQuote] = useState('Loading...');
+  const [Author, setAuthor] = useState('Loading...');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+
+  const randomQuote = () => {
+    setIsLoading(true);
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+     
+      setQuote(result.content);
+      setAuthor(result.author);
+      setIsLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    randomQuote();
+  }, []);
+
+  
+
+ 
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5372F0',
+      }}>
+        <StatusBar barStyle="light-content" />
+      <View
+        style={{
+          width: '90%',
+          backgroundColor: '#fff',
+          borderRadius: 20,
+          padding: 20,
+        }}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 26,
+            fontWeight: '600',
+            color: '#333',
+            marginBottom: 20,
+          }}>
+          Quote of the Day
+        </Text>
+        <FontAwesome5
+          name="quote-left"
+          style={{fontSize: 20, marginBottom: -12}}
+          color="#000"
+        />
+        <Text
+          style={{
+            color: '#000',
+            fontSize: 16,
+            lineHeight: 26,
+            letterSpacing: 1.1,
+            fontWeight: '400',
+            textAlign: 'center',
+            marginBottom: 10,
+            paddingHorizontal: 30,
+          }}>
+          {Quote}
+        </Text>
+        <FontAwesome5
+          name="quote-right"
+          style={{
+            fontSize: 20,
+            textAlign: 'right',
+            marginTop: -20,
+            marginBottom: 20,
+          }}
+          color="#000"
+        />
+        <Text
+          style={{
+            textAlign: 'right',
+            fontWeight: '300',
+            fontStyle: 'italic',
+            fontSize: 16,
+            color: '#000',
+          }}>
+          —— {Author}
+        </Text>
+        <TouchableOpacity
+          onPress={randomQuote}
+          style={{
+            backgroundColor: isLoading ? 'rgba(83, 114, 240, 0.7)' : 'rgba(83, 114, 240, 1)',
+            padding: 20,
+            borderRadius: 30,
+            marginVertical: 20,
+          }}>
+          <Text style={{color: '#fff', fontSize: 18, textAlign: 'center'}}>
+            {isLoading ? "Loading..." : "New Quote"}
+          </Text>
+        </TouchableOpacity>
+
+        <View style={{flexDirection: 'row',justifyContent:'space-around'}}>
+        
+          
+          
+        </View>
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
